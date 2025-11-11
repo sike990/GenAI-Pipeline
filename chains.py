abd40d046d2f431c9f6ac98cf5b2bcedc528688e
Anush Jain <120271799+sike990@users.noreply.github.com>
2025-11-11 21:27:14 +0530

    Made prompting changes

--- chains.py
+++ chains.py
@@ -43,12 +43,12 @@ class Chain:
             the seamless integration of business processes through automated tools. 
             Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
             process optimization, cost reduction, and heightened overall efficiency. 
-            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of AtliQ 
+            Your job is to write a Latex code for resume for the job mentioned above describing the capability of AtliQ 
             in fulfilling their needs.
             Also add the most relevant ones from the following links to showcase Atliq's portfolio: {link_list}
             Remember you are Mohan, BDE at AtliQ. 
             Do not provide a preamble.
-            ### EMAIL (NO PREAMBLE):
+            ### LATEX (NO PREAMBLE):
 
             """
         )
